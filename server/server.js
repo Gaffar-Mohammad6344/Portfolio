f5f593d4452d6c485bb7a43e70074ba2900f3470
MounikaPedalenka <mounikapedalenka273@gmail.com>
2026-07-12 00:26:17 +0530

    deploy

--- server/server.js
+++ server/server.js
@@ -156,5 +156,6 @@ app.post("/api/contact", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`\n🚀 Server running on http://localhost:${PORT}\n`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`\n🚀 Server running on http://localhost:${PORT}\n`));
+module.exports = app;
